2712c75a460247ae8d9c946032285fa362c380f5
Mark Bucklin <markbucklin@gmail.com>
2018-01-24 19:14:56 -0500

    adding devicemanager

---
+++ src/DeviceLib/devicemanager.h
@@ -0,0 +1,14 @@
+/*
+  devicemanager.h - Library for managing the operation modular nodes on an embedded computing device
+  Created by Mark Bucklin, Jan 21, 2018.
+*/
+
+#ifndef DEVICEMANAGER_h
+#define DEVICEMANAGER_h
+
+#include <Arduino.h>
+
+
+
+
+#endif DEVICEMANAGER_h
