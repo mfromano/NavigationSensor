2ff133055acd83977371c6daa4ae05b7d28b27c8
Mark Bucklin <markbucklin@gmail.com>
2018-02-08 06:25:03 -0500

    added multiple options for timing: TimerOne, TimerThree, FrequencyTimer2

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,16 @@
 #include <elapsedMillis.h>
 
 #if defined(__MK20DX128__) || defined(__MK20DX256__)
-#include <usb_serial.h> // Teensy 3.0 and 3.1
+#include <usb_serial.h>  // Teensy 3.0 and 3.1
 #define SERIAL_CLASS usb_serial_class
 #elif defined(_SAM3XA_)
-#include <UARTClass.h> // Arduino Due
+#include <UARTClass.h>  // Arduino Due
 #define SERIAL_CLASS UARTClass
 #elif defined(USE_USBCON)
 // Arduino Leonardo USB Serial Port
 #define SERIAL_CLASS Serial_
 #else
-#include <HardwareSerial.h> // Arduino AVR
+#include <HardwareSerial.h>  // Arduino AVR
 #define SERIAL_CLASS HardwareSerial
 #endif
 
@@ -41,7 +41,7 @@
 #define CAMERA_FPS 40
 #define SAMPLES_PER_CAMERA_FRAME 12
 
-//Heart-Beat Settings
+// Heart-Beat Settings
 #define HEARTBEAT_PERIOD_MILLIS 1000
 #define HEARTBEAT_OUTPUT '\0'
 
@@ -53,17 +53,15 @@
 #define FIXED_SIZE_DATA_DELIM ','
 #define FIXED_SIZE_MSG_TERMINATOR '\t'
 
-enum class TransmitFormat
-{
+enum class TransmitFormat {
   FIXED,
   DELIMITED,
   JSON,
-  BINARY // todo binary stream
+  BINARY  // todo binary stream
 };
 
 // Define Left-Right Sensor Pair Structure
-typedef struct
-{
+typedef struct {
   ADNS &left;
   ADNS &right;
 } sensor_pair_t;
@@ -72,28 +70,47 @@ typedef struct
 ADNS adnsA(CS_PIN_A);
 ADNS adnsB(CS_PIN_B);
 sensor_pair_t sensor = {adnsA, adnsB};
-typedef struct
-{
+typedef struct {
   char id = 'L';
   displacement_t p;
 } labeled_sample_t;
 
+// // Trigger-Output Settings and Implementation
+// #include <TimerOne.h>
+// #define TRIGGER_TIMER Timer1
+// // PWM pins: {teensy31: [3,4], uno:[9,10], mega:[11,12,13]}
+// #define TRIGGER_PWM_PIN1 3
+// #define TRIGGER_PWM_PIN2 4
+
+// #include <TimerThree.h>
+// #define TRIGGER_TIMER Timer3
+// mega:[2,3,5]} todo: replace with generic trigger functionality
+//// PWM pins: {teensy31: [25,32], tensy35/6:[29,30],
+// #define TRIGGER_PWM_PIN1 25
+// #define TRIGGER_PWM_PIN2 32
+
+// Synchronous Trigger Rates -> Primary, secondary, ...
+
+// Use zero-jitter & cross-platform Frequency-Timer-2 library
+#include <FrequencyTimer2.h>
+
 // Specify some Constants for Timing and Unit Conversion/Reporting
-const int32_t DISPLACEMENT_FPS = (CAMERA_FPS * SAMPLES_PER_CAMERA_FRAME);
-const uint32_t usLoop = 1e6 / DISPLACEMENT_FPS;
+const int32_t DISPLACEMENT_SAMPLE_RATE =
+    (CAMERA_FPS * SAMPLES_PER_CAMERA_FRAME);
 volatile int syncEveryNCount = SAMPLES_PER_CAMERA_FRAME;
+// const uint32_t masterClkPeriodMicros = 1e6 / DISPLACEMENT_SAMPLE_RATE;
+
+// Initialize microsecond counter track elapsed time
+struct {
+  elapsedMicros session;
+  elapsedMicros camera_frame;
+  elapsedMicros motion_sample;
+} timeSinceStart;
 
 // Delimiter & Precision for Conversion to String
-<<<<<<< HEAD
 const TransmitFormat format = TransmitFormat::FIXED;
-const unit_specification_t units = {
-    Unit::Distance::MICROMETER,
-    Unit::Time::MICROSECOND};
-=======
-const TransmitFormat format = TransmitFormat::DELIMITED;
 const unit_specification_t units = {Unit::Distance::MICROMETER,
                                     Unit::Time::MICROSECOND};
->>>>>>> capnproto
 const char delimiter = '\t';
 const unsigned char decimalPlaces = 3;
 const bool waitBeforeStart = true;
@@ -108,13 +125,11 @@ const String flatFieldNames[] = {
     sensorNames[0] + '_' + fieldNames[2], sensorNames[1] + '_' + fieldNames[0],
     sensorNames[1] + '_' + fieldNames[1], sensorNames[1] + '_' + fieldNames[2]};
 
-// Initialize microsecond counter and sample buffer
-elapsedMicros usCnt;
+// Initialize sample buffer
 CircularBuffer<labeled_sample_t, 3> bufA;
 CircularBuffer<labeled_sample_t, 3> bufB;
-uint32_t sensorSampleCnt = 0;
-uint32_t cameraFrameCnt = 0;
-uint32_t sampleCntTarget = 0;
+uint32_t cameraFrameAcquiredCount = 0;
+uint32_t sampleCountRemaining = 0;
 
 // Declare Test Functions
 static inline void checkState();
@@ -127,54 +142,24 @@ void transmitDisplacementBinary(const labeled_sample_t);
 void transmitDisplacementDelimitedString(const labeled_sample_t);
 void transmitDisplacementFixedSize(const labeled_sample_t);
 
-enum ControllerState
-{
-    SETUP,
-    WAIT,
-    RUN
-} controllerState;
-enum SampleState
-{
-    NONE,
-    CAPTURE, // await sensor integration
-    ACQUIRE,
-    TRANSMIT
+enum ControllerState { SETUP, WAIT, RUN } controllerState;
+enum SampleState {
+  NONE,
+  CAPTURE,  // await sensor integration
+  ACQUIRE,
+  TRANSMIT
 } sampleState;
 
 // =============================================================================
 //   INITIALIZATION
 // =============================================================================
-void setup()
-{
-<<<<<<< HEAD
-    controllerState = SETUP;
+void setup() {
+  controllerState = SETUP;
 
-    // Begin Serial
-    Serial.begin(115200);
-    while (!Serial)
-    {
-        ; // may only be needed for native USB port
-    }
-    delay(10);
-
-    // Begin Sensors
-    sensor.left.begin();
-    delay(30);
-    sensor.right.begin();
-    delay(30);
-
-    // Set Sync Out Pin Modes
-    fastDigitalWrite(SYNC_OUT_PIN, !SYNC_PULSE_STATE);
-    fastDigitalWrite(SYNC_EVERY_N_PIN, !SYNC_PULSE_STATE);
-    fastPinMode(SYNC_OUT_PIN, OUTPUT);
-    fastPinMode(SYNC_EVERY_N_PIN, OUTPUT);
-}
-=======
   // Begin Serial
   Serial.begin(115200);
-  while (!Serial)
-  {
-    ; // may only be needed for native USB port
+  while (!Serial) {
+    ;  // may only be needed for native USB port
   }
   delay(10);
 
@@ -190,183 +175,107 @@ void setup()
   fastPinMode(SYNC_OUT_PIN, OUTPUT);
   fastPinMode(SYNC_EVERY_N_PIN, OUTPUT);
 
-  // Print units and Fieldnames (header)
-  sendFormat();
-
-  checkCmd();
-
-  // Start Acquisition
-  usCnt = 0;
-  sensor.left.triggerAcquisitionStart();
-  sensor.right.triggerAcquisitionStart();
-
-  // Send Sync-Every-N Pulse (at start of first and every N subsequent frames)
-  fastDigitalWrite(SYNC_OUT_PIN, SYNC_PULSE_STATE);
-  fastDigitalWrite(SYNC_EVERY_N_PIN, SYNC_PULSE_STATE);
-  syncEveryNCount = SAMPLES_PER_CAMERA_FRAME;
-};
->>>>>>> capnproto
+  // Setup Sync/Trigger-Output Timing
+  // Timer1.initialize(masterClkPeriodMicros);
+  FrequencyTimer2::setPeriod(1e6 / DISPLACEMENT_SAMPLE_RATE)
+}
 
 // =============================================================================
 //   LOOP
 // =============================================================================
-void loop()
-{
-<<<<<<< HEAD
-    checkState();
-    sendAnyUpdate();
-=======
+void loop() {
+  checkState();
   sendAnyUpdate();
->>>>>>> capnproto
 
   // Reset Trigger Outputs
   static bool needSyncOutReset = true;
-  while (usCnt < usLoop)
-  {
-    if (needSyncOutReset && (usCnt > SYNC_PULSE_WIDTH_MICROS))
-    {
+  while (timeSinceStart.acquisition; < masterClkPeriodMicros) {
+    if (needSyncOutReset &&
+        (timeSinceStart.acquisition; > SYNC_PULSE_WIDTH_MICROS)) {
       fastDigitalWrite(SYNC_OUT_PIN, !SYNC_PULSE_STATE);
       fastDigitalWrite(SYNC_EVERY_N_PIN, !SYNC_PULSE_STATE);
       needSyncOutReset = false;
     }
   }
 
-<<<<<<< HEAD
-    // Set Downsampled ("Camera") Trigger Output Every N Samples
-    if (--syncEveryNCount <= 0)
-    {
-        fastDigitalWrite(SYNC_EVERY_N_PIN, SYNC_PULSE_STATE);
-        syncEveryNCount = SAMPLES_PER_CAMERA_FRAME;
-    }
-=======
-  checkCmd();
-
   // Set Downsampled ("Camera") Trigger Output Every N Samples
-  if (--syncEveryNCount <= 0)
-  {
+  if (--syncEveryNCount <= 0) {
     fastDigitalWrite(SYNC_EVERY_N_PIN, SYNC_PULSE_STATE);
     syncEveryNCount = SAMPLES_PER_CAMERA_FRAME;
   }
->>>>>>> capnproto
 
   // Initiate Sample Capture
   fastDigitalWrite(SYNC_OUT_PIN, SYNC_PULSE_STATE);
   captureDisplacement();
-  usCnt -= usLoop; // usCnt = totalLag?
+  timeSinceStart.acquisition;
+  -= masterClkPeriodMicros;  // timeSinceStart.acquisition;= totalLag?
 
   needSyncOutReset = true;
 }
 
-static inline void checkState()
-{
-<<<<<<< HEAD
-    // Controller State
-    switch (controllerState)
-    {
+static inline void checkState() {
+  // Controller State
+  switch (controllerState) {
     case (SETUP):
+      if (waitBeforeStart == false) {
         // Set initial sample-count to acquire immediately after setup
-        if (waitBeforeStart == false)
-        {
-            sampleCntTarget = UINT32_MAX;
-            startAcquisisition();
-            controllerState = RUN;
-        }
-        else
-        {
-            controllerState = WAIT;
-        }
-    case (WAIT):
-        while (!Serial.available())
-        {
-            sendHeartBeat();
-        }
-        checkCmd();
+        sampleCountRemaining = UINT32_MAX;
+        startAcquisisition();
         controllerState = RUN;
-
+      } else {
+        // Enter WAIT mode
+        controllerState = WAIT;
+      }
+    case (WAIT):
+      // Send heart-beat to indicate status as ready/waiting for client cmd
+      while (!Serial.available()) {
+        sendHeartBeat();
+      }
+      checkCmd();
+      controllerState = RUN;
     case (RUN):
-    }
-    checkCmd();
+  }
+  checkCmd();
 
-    sampleCntTarget--;
+  sampleCountRemaining--;
 }
 
-static inline void sendHeartBeat()
-{
-    static elapsedMillis heartBeatMillis = 0;
-    if (heartBeatMillis > HEARTBEAT_PERIOD_MILLIS)
-    {
-        Serial.print(HEARTBEAT_OUTPUT);
-        heartBeatMillis -= HEARTBEAT_PERIOD_MILLIS;
-    }
-=======
-  const String dunit = getAbbreviation(units.distance);
-  const String tunit = getAbbreviation(units.time);
-  if (format == TransmitFormat::FIXED)
-  {
-    Serial.println("\n\n\n");
-    Serial.println("label\t" + dunit + "\t\t" + dunit + "\t\t" + tunit + "\t" +
-                   "label\t" + dunit + "\t\t" + dunit + "\t\t" + tunit);
+static inline void sendHeartBeat() {
+  static elapsedMillis heartBeatMillis = 0;
+  if (heartBeatMillis > HEARTBEAT_PERIOD_MILLIS) {
+    Serial.print(HEARTBEAT_OUTPUT);
+    heartBeatMillis -= HEARTBEAT_PERIOD_MILLIS;
   }
-  else
-  {
-
-    Serial.print(String(
-        flatFieldNames[0] + " [" + dunit + "]" + delimiter + flatFieldNames[1] +
-        " [" + dunit + "]" + delimiter + flatFieldNames[2] + " [" + tunit +
-        "]" + delimiter + flatFieldNames[3] + " [" + dunit + "]" + delimiter +
-        flatFieldNames[4] + " [" + dunit + "]" + delimiter + flatFieldNames[5] +
-        " [" + tunit + "]" + delimiter + '\n'));
-  }
->>>>>>> capnproto
 }
 
-static inline void checkCmd()
-{
-<<<<<<< HEAD
-    // Read Serial to see if request for more frames has been sent
-    if (Serial.available())
-    {
-        int32_t moreFramesCnt = Serial.parseInt();
-        sampleCntTarget += (SAMPLES_PER_CAMERA_FRAME * moreFramesCnt);
-        //todo pause
-=======
+static inline void checkCmd() {
   // Read Serial to see if request for more frames has been sent
-  if (waitBeforeStart)
-  {
-    if (Serial.available() || (sampleCntTarget < 0))
-    {
-      while (!Serial.available())
-        delayMicroseconds(100);
-
-      uint32_t moreFramesCnt = Serial.parseInt();
-      sampleCntTarget += (SAMPLES_PER_CAMERA_FRAME * moreFramesCnt);
-      // todo pause
->>>>>>> capnproto
-    }
-    sampleCntTarget--;
+  if (Serial.available()) {
+    int32_t moreFramesCnt = Serial.parseInt();
+    sampleCountRemaining += (SAMPLES_PER_CAMERA_FRAME * moreFramesCnt);
+    // todo pause
   }
 }
 
-static inline void startAcquisition()
-{
-    // Print units and Fieldnames (header)
-    sendFormat();
+static inline void startAcquisition() {
+  // Print units and Fieldnames (header)
+  sendFormat();
 
-    // Reset elapsed microsecond timer for sample duration timing
-    usCnt = 0;
+  // Reset elapsed microsecond timer for sample duration timing
+  timeSinceStart.acquisition;
+  = 0;
 
-    // Trigger start using class methods in ADNS library
-    sensor.left.triggerAcquisitionStart();
-    sensor.right.triggerAcquisitionStart();
+  // Trigger start using class methods in ADNS library
+  sensor.left.triggerAcquisitionStart();
+  sensor.right.triggerAcquisitionStart();
 
-    // Send Sync & "Sync-Every-N" Pulse (for camera) at start of first frame
-    fastDigitalWrite(SYNC_OUT_PIN, SYNC_PULSE_STATE);
-    fastDigitalWrite(SYNC_EVERY_N_PIN, SYNC_PULSE_STATE);
-    syncEveryNCount = SAMPLES_PER_CAMERA_FRAME;
+  // Send Sync & "Sync-Every-N" Pulse (for camera) at start of first frame
+  fastDigitalWrite(SYNC_OUT_PIN, SYNC_PULSE_STATE);
+  fastDigitalWrite(SYNC_EVERY_N_PIN, SYNC_PULSE_STATE);
+  syncEveryNCount = SAMPLES_PER_CAMERA_FRAME;
 }
 
-static inline void captureDisplacement()
-{
+static inline void captureDisplacement() {
   sensor.left.triggerSampleCapture();
   sensor.right.triggerSampleCapture();
   const labeled_sample_t sampleA = {'L', sensor.left.readDisplacement(units)};
@@ -375,63 +284,51 @@ static inline void captureDisplacement()
   bufB.push(sampleB);
 }
 
-static inline void sendAnyUpdate()
-{
+static inline void sendAnyUpdate() {
   // Print Velocity
-  while ((!bufA.isEmpty()) && (!(bufB.isEmpty())))
-  {
-    switch (format)
-    {
-    case (TransmitFormat::FIXED):
-      transmitDisplacementFixedSize(bufA.shift());
-      transmitDisplacementFixedSize(bufB.shift());
-      Serial.write('\r');
-      break;
-    case (TransmitFormat::DELIMITED):
-      transmitDisplacementDelimitedString(bufA.shift());
-      transmitDisplacementDelimitedString(bufB.shift());
-      Serial.print('\n');
-      break;
-    case (TransmitFormat::BINARY):
-      transmitDisplacementBinary(bufA.shift());
-      transmitDisplacementBinary(bufB.shift());
-      break;
-    case (TransmitFormat::JSON):
-      // todo
-      break;
-    default:
-      break;
+  while ((!bufA.isEmpty()) && (!(bufB.isEmpty()))) {
+    switch (format) {
+      case (TransmitFormat::FIXED):
+        transmitDisplacementFixedSize(bufA.shift());
+        transmitDisplacementFixedSize(bufB.shift());
+        Serial.write('\r');
+        break;
+      case (TransmitFormat::DELIMITED):
+        transmitDisplacementDelimitedString(bufA.shift());
+        transmitDisplacementDelimitedString(bufB.shift());
+        Serial.print('\n');
+        break;
+      case (TransmitFormat::BINARY):
+        transmitDisplacementBinary(bufA.shift());
+        transmitDisplacementBinary(bufB.shift());
+        break;
+      case (TransmitFormat::JSON):
+        // todo
+        break;
+      default:
+        break;
     }
   }
 }
 
-static inline void sendFormat()
-{
-    const String dunit = getAbbreviation(units.distance);
-    const String tunit = getAbbreviation(units.time);
-    if (format == TransmitFormat::FIXED)
-    {
-        Serial.println("\n\n\n");
-        Serial.println("label\t" + dunit + "\t\t" + dunit + "\t\t" + tunit + "\t" +
-                       "label\t" + dunit + "\t\t" + dunit + "\t\t" + tunit);
-    }
-    else
-    {
-
-        Serial.print(
-            String(
-                flatFieldNames[0] + " [" + dunit + "]" + delimiter +
-                flatFieldNames[1] + " [" + dunit + "]" + delimiter +
-                flatFieldNames[2] + " [" + tunit + "]" + delimiter +
-                flatFieldNames[3] + " [" + dunit + "]" + delimiter +
-                flatFieldNames[4] + " [" + dunit + "]" + delimiter +
-                flatFieldNames[5] + " [" + tunit + "]" + delimiter +
-                '\n'));
-    }
+static inline void sendFormat() {
+  const String dunit = getAbbreviation(units.distance);
+  const String tunit = getAbbreviation(units.time);
+  if (format == TransmitFormat::FIXED) {
+    Serial.println("\n\n\n");
+    Serial.println("label\t" + dunit + "\t\t" + dunit + "\t\t" + tunit + "\t" +
+                   "label\t" + dunit + "\t\t" + dunit + "\t\t" + tunit);
+  } else {
+    Serial.print(String(
+        flatFieldNames[0] + " [" + dunit + "]" + delimiter + flatFieldNames[1] +
+        " [" + dunit + "]" + delimiter + flatFieldNames[2] + " [" + tunit +
+        "]" + delimiter + flatFieldNames[3] + " [" + dunit + "]" + delimiter +
+        flatFieldNames[4] + " [" + dunit + "]" + delimiter + flatFieldNames[5] +
+        " [" + tunit + "]" + delimiter + '\n'));
+  }
 }
 
-void transmitDisplacementBinary(const labeled_sample_t sample)
-{
+void transmitDisplacementBinary(const labeled_sample_t sample) {
   // todo not working -> serialize with standard library
   // cast floats to bytes todo: make architecture invariant
   const size_t numBytes = 12;
@@ -441,10 +338,8 @@ void transmitDisplacementBinary(const labeled_sample_t sample)
     uint32_t base;
   } f2u;
   const f2u dxyt[3] = {sample.p.dx, sample.p.dy, sample.p.dt};
-  for (int f = 0; f < 3; f++)
-  {
-    for (int b = 0; b < 4; b++)
-    {
+  for (int f = 0; f < 3; f++) {
+    for (int b = 0; b < 4; b++) {
       *(buf + (f * b)) = ((dxyt[3 - f].base) >> (8 * b)) & 0xFF;
     }
   }
@@ -452,8 +347,7 @@ void transmitDisplacementBinary(const labeled_sample_t sample)
 }
 
 //  Variable-Size Conversion to ASCII Strings
-void transmitDisplacementDelimitedString(const labeled_sample_t sample)
-{
+void transmitDisplacementDelimitedString(const labeled_sample_t sample) {
   // Convert to String class
   const String dx = String(sample.p.dx, decimalPlaces);
   const String dy = String(sample.p.dy, decimalPlaces);
@@ -464,8 +358,7 @@ void transmitDisplacementDelimitedString(const labeled_sample_t sample)
 }
 
 // Fixed-Size Buffer Conversion to ASCII char array
-void transmitDisplacementFixedSize(const labeled_sample_t sample)
-{
+void transmitDisplacementFixedSize(const labeled_sample_t sample) {
   static const signed char width = ((CHAR_BUFFER_NUM_BYTES - 2) / 3) - 2;
   static const size_t increment = width + 2;
 
