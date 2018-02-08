6a0b5268d13a7384b92b6437dda74e86819708da
Mark Bucklin <markbucklin@gmail.com>
2018-02-08 06:51:25 -0500

    moved defines and enums from top of main.cpp to new main_config.h

--- src/main.cpp
+++ src/main.cpp
@@ -14,43 +14,7 @@
 // Include ADNS Library for ADNS-9800 Sensor
 #include "ADNS9800\ADNS.h"
 
-// Pin Settings
-#define CS_PIN_A 4
-#define CS_PIN_B 5
-#define SYNC_OUT_PIN 3
-#define SYNC_EVERY_N_PIN 6
-#define SYNC_PULSE_WIDTH_MICROS 500
-#define SYNC_PULSE_STATE HIGH
-
-// Timing Settings (shooting for 480 fps minimum, sync with camera is nominal at
-// this juncture)
-#define CAMERA_FPS 40
-#define SAMPLES_PER_CAMERA_FRAME 12
-
-// Heart-Beat Settings
-#define HEARTBEAT_PERIOD_MILLIS 1000
-#define HEARTBEAT_OUTPUT '\0'
-
-// Buffer Size (comment out CHAR_BUFFER_SIZE_FIXED to try variable size mode)
-#define CHAR_BUFFER_NUM_BYTES 44
-
-// Delimiters and Terminators
-#define FIXED_SIZE_ID_DELIM ':'
-#define FIXED_SIZE_DATA_DELIM ','
-#define FIXED_SIZE_MSG_TERMINATOR '\t'
-
-enum class TransmitFormat {
-  FIXED,
-  DELIMITED,
-  JSON,
-  BINARY  // todo binary stream
-};
-
-// Define Left-Right Sensor Pair Structure
-typedef struct {
-  ADNS &left;
-  ADNS &right;
-} sensor_pair_t;
+#include "main_config.h"
 
 // Create Sensor Objects with Specified Slave-Select Pins
 ADNS adnsA(CS_PIN_A);
