291593b25f2d80538b9bf0713b850c65a728d7c7
Mark Bucklin <markbucklin@gmail.com>
2018-02-14 17:16:37 -0500

    new files added

---
+++ src/DeviceLib/timestamp.h
@@ -0,0 +1,17 @@
+/*
+  timestamp.h
+  -> Common include for data timestamp
+*/
+
+#ifndef TIMESTAMP_h
+#define TIMESTAMP_h
+
+#include <Arduino.h>
+#include <Time.h>
+#include <stdint.h>
+#include <chrono>
+
+// std::chrono::
+
+
+#endif
